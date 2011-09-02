7896d0a824768c1a3cdd9b47f2125bb67932ee14
takano32 <mitsuhiro.takano@gree.co.jp>
2011-09-02 16:40:05 +0900

    solved 'Web Game'

--- webgame_hint/solver.js
+++ webgame_hint/solver.js
@@ -1,5 +1,4 @@
 // vim: set et sts=2 sw=2 ts=2 fdm=marker ft=javascript :
-
 /*
 var element = document.getElementById('card0');
 if (element == null) {
@@ -12,7 +11,6 @@ if (element == null) {
 }
 */
 
-
 var num = 0;
 var color = [];
 while ((element = document.getElementById('card' + num)) != null) {
@@ -23,13 +21,24 @@ while ((element = document.getElementById('card' + num)) != null) {
   num++;
 }
 
+function cardOpen(i) {
+  var element = document.getElementById('card' + i);
+  var myevent = document.createEvent('MouseEvents');
+  myevent.initEvent('click', false, true);
+  element.dispatchEvent(myevent);
+  color[num] = element.style.backgroundColor;
+}
+
 var end = num;
 for (var i = 0; i < end; i++) {
   for (var j = 0; j < end; j++) {
-    // alert('Card color is "' + color[i] + '".');
     if (i == j) {
       continue;
     } else {
+      if (color[i] == color[j]) {
+        cardOpen(i);
+        cardOpen(j);
+      }
     }
   }
 }
