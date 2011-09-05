f40bdff44fb15e9cba0348c61665b6e19494da7f
takano32 <mitsuhiro.takano@gree.co.jp>
2011-09-05 13:33:43 +0900

    add adjacent array to Board

--- SlidingPuzzle/board.rb
+++ SlidingPuzzle/board.rb
@@ -10,11 +10,32 @@ class SlidingPuzzle::Board
 	def initialize(width, height, board)
 		@width = width
 		@height = height
-		@board = board.scan(%r!(.{#{@width}})!).flatten(1)
+		@size = @width * @height
+		@board = board.scan(%r!(.{#{width}})!).flatten(1)
+		@adjacent = []
+		(0...@height).each do |y|
+			(0...@width).each do |x|
+				#u = (y - 1) * @width + x
+				#d = (y + 1) * @width + x
+				#l = y * @width + (x - 1)
+				#r = y * @width + (x + 1)
+				a = []
+				#a << u if 0 <= u and u < @size
+				#a << d if 0 <= d and d < @size
+				#a << l if 0 <= l and l < @size
+				#a << r if 0 <= r and r < @size
+				@adjacent << a
+			end
+		end
+	end
+
+	def solve
 	end
 end
 
 if __FILE__ == $0 then
-	server = DRbObject.new_with_uri('druby://localhost:12345')
-	p server.board(5000)
+	#server = DRbObject.new_with_uri('druby://localhost:12345')
+	#p server.board(1)
+	b = SlidingPuzzle::Board.new(5, 6, '12=E4D9HIF8=GN576LOABMTPKQSR0J')
+	p b
 end
